46b802f942db81ed931950cdd758e7580f82b511
Tanner Richards <Tanner.richards990@gmail.com>
2022-12-05 15:15:23 -0800

    moved login test to test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,29 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+jest.mock('../lib/services/githubService.js');
+const agent = request.agent(app);
+
+describe('github-oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
-  afterAll(() => {
-    pool.end();
+
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const response = await agent
+      .get('/api/v1/github/login')
+      .redirects(1);
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      username: 'test',
+      email: 'test@test.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number)
+    }
+    );
   });
 });
