5204a51f3aceee6a7a2f8ce6e00489209c0cd5c0
Tanner Richards <Tanner.richards990@gmail.com>
2022-12-06 12:25:53 -0800

    running through all the errors to try to get a single test to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,19 +2,38 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-
 jest.mock('../lib/services/github');
-const agent = request.agent(app);
+
 
 describe('github-oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+  it('should login and redirect users to /api/v1/github/oauth', async () => {
     const res = await request(app).get('/api/v1/github/login');
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+
     );
+  });
+  
+  it('/api/v1/github/callback should login users and redirect to dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      login: 'fake_github_user',
+      email: 'faker@example.com',
+      avatar: 'https://www.placecage.com/gif/200/300',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
   });
 });
