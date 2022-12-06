ccd7e964102ed91b582c08f7ee61c4b343561753
Tanner Richards <Tanner.richards990@gmail.com>
2022-12-06 14:41:03 -0800

    built out controller for getting all post route

---
+++ __tests__/posts.test.js
@@ -0,0 +1,19 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/github');
+const agent = request.agent(app);
+
+describe('post routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('get allows authenticated user to see posts', async () => {
+    await agent.get('/api/v1/github/callback?code=42');
+    const res = await agent.get('/api/v1/posts');
+    expect(res.status).toEqual(200);
+  });
+}); 
